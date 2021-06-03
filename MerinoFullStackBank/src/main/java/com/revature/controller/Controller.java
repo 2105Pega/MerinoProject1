65237342ca94ff67a056469f5125924c7d2cb945
Luis Merino <luis.pablo.merino.m@gmail.com>
2021-06-03 15:31:42 -0400

    Setup server and fixed web.xml for controller to work

--- MerinoFullStackBank/src/main/java/com/revature/controller/Controller.java
+++ MerinoFullStackBank/src/main/java/com/revature/controller/Controller.java
@@ -1,5 +1,17 @@
 package com.revature.controller;
 
-public class Controller {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Path("/controller")
+public class Controller {
+	@Path("/hello")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
 }
